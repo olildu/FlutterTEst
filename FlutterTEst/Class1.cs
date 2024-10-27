239cafc8d925231bd73d7699843ed061a94d50e3
olildu <ebinsanthosh06@gmail.com>
2024-10-28 00:14:59 +0530

    Add project files.

---
+++ FlutterTEst/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlutterTEst
+{
+    public sealed class HelloWorld
+    {
+        public static string GetMessage()
+        {
+            return "Hello, World from C#!";
+        }
+    }
+}
